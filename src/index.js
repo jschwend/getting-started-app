f46e9f23e4a880f2b2fe70bb0f57c78a7e3b241c
Jack Schwenderman <jack_schwenderman@mckinsey.com>
2024-02-29 12:13:20 -0500

    Added Heroku files

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+        port = 8000;
+    }
+    app.listen(port, () => console.log('Listening on port ' + port));
 }).catch((err) => {
     console.error(err);
     process.exit(1);
